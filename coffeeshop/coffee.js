63780791ee6ef00565f89dbd93993ca6cbb8b443
Clarice Kim <claricek@uchicago.edu>
2024-10-06 12:05:55 -0500

    tried to implement hover

--- coffeeshop/coffee.js
+++ coffeeshop/coffee.js
@@ -1,12 +1,8 @@
 const coffeevideo = document.querySelector("#coffee-video");
 const squares = document.querySelectorAll(".square");
-squares.forEach(square => {
-  square.style.display = "none";
-});
-const text = document.querySelectorAll("h3");
-text.forEach(tex => {
-    tex.style.display = "none";
-});
+// squares.forEach(square => {
+//   square.style.display = "none";
+// });
 
 const determinedvideo = document.querySelector("#determined-video");
 
@@ -26,43 +22,9 @@ coffeevideo.addEventListener("ended", (event) => {
     squares.forEach(square => {
         square.style.display = "block";
     });
-    text.forEach(tex => {
-        tex.style.display = "block";
-    });
 
   });
 
-const lsquare = document.getElementById("lsquare");
-const csquare = document.getElementById("csquare");
-const rsquare = document.getElementById("rsquare");
 
-const long_lsquare = document.getElementById("leftclick");
-const long_csquare = document.getElementById("centerclick");
-const long_rsquare = document.getElementById("rightclick");
-
-lsquare.addEventListener("click", function() {
-    long_lsquare.style.display = "block";
-});
-
-csquare.addEventListener("click", function() {
-    long_csquare.style.display = "block";
-});
-
-rsquare.addEventListener("click", function() {
-    long_rsquare.style.display = "block";
-});
-
-//Close windows
-long_lsquare.addEventListener("click", function() {
-    long_lsquare.style.display = "none";
-});
-
-long_csquare.addEventListener("click", function() {
-    long_csquare.style.display = "none";
-});
-
-long_rsquare.addEventListener("click", function() {
-    long_rsquare.style.display = "none";
-});
 
 
