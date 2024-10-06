f193dd1f7791510bd1e0060ad09bcb17d1648a96
Clarice Kim <claricek@uchicago.edu>
2024-10-05 23:38:30 -0500

    it wokrs

--- coffeeshop/coffee.js
+++ coffeeshop/coffee.js
@@ -1,12 +1,12 @@
 const coffeevideo = document.querySelector("#coffee-video");
-// const squares = document.querySelector(".square")
-// squares.style.display = "none";
+const squares = document.querySelector(".square")
+squares.style.display = "none";
 
 coffeevideo.addEventListener("ended", (event) => {
     console.log(
       "Video stopped coffee scene",
     );
-    // squares.style.display = "block";
+    squares.style.display = "block";
 
   });
   
