4f45bd59a98c14f84fedc23bdfd15423a45e3d0d
Clarice Kim <claricek@uchicago.edu>
2024-10-06 00:13:37 -0500

    added clickme sign

--- coffeeshop/coffee.js
+++ coffeeshop/coffee.js
@@ -1,12 +1,36 @@
 const coffeevideo = document.querySelector("#coffee-video");
-const squares = document.querySelector(".square")
-squares.style.display = "none";
+const squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+  square.style.display = "none";
+});
 
 coffeevideo.addEventListener("ended", (event) => {
     console.log(
       "Video stopped coffee scene",
     );
-    squares.style.display = "block";
+    squares.forEach(square => {
+        square.style.display = "block";
+    });
 
   });
-  
+
+const lsquare = document.getElementById("lsquare");
+const csquare = document.getElementById("csquare");
+const rsquare = document.getElementById("rsquare");
+
+const long_lsquare = document.getElementById("long-lsquare");
+const long_csquare = document.getElementById("long-csquare");
+const long_rsquare = document.getElementById("long-rsquare");
+
+// lsquare.addEventListener("mouseover", function() {
+//     long_lsquare.style.display = "block";
+// });
+
+// csquare.addEventListener("mouseover", function() {
+//     long_csquare.style.display = "block";
+// });
+
+// rsquare.addEventListener("mouseover", function() {
+//     long_rsquare.style.display = "block";
+// });
+
